2079d7c12be43775000157b52cd9e1155eb2bb03
Prathishta Shetty <prathishta.shetty123@gmail.com>
2026-05-09 12:44:03 +0530

    Create script.js

---
+++ assignment11/script.js
@@ -0,0 +1,12 @@
+function changeLayer() {
+
+  const red = document.querySelector(".red");
+  const blue = document.querySelector(".blue");
+  const green = document.querySelector(".green");
+
+  // Change z-index values
+  red.style.zIndex = 5;
+  blue.style.zIndex = 1;
+  green.style.zIndex = 2;
+
+}
