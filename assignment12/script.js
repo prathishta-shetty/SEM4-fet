7ae19ea5a9cafa21fbdcb147946baa727247112e
Prathishta Shetty <prathishta.shetty123@gmail.com>
2026-05-15 18:51:09 +0530

    Update script.js

--- assignment12/script.js
+++ assignment12/script.js
@@ -1,25 +1,28 @@
 
-function showNumbers() {
+function countStudents() {
 
-    // While Loop
     let i = 1;
-    let whileText = "While Loop: ";
+    let whileText = "Students Present (While Loop): ";
 
     while(i <= 5) {
-        whileText += i + " ";
+
+        whileText += "Student" + i + " ";
+
         i++;
     }
 
     document.getElementById("whileResult").innerHTML = whileText;
 
 
-    // Do While Loop
     let j = 1;
-    let doText = "Do While Loop: ";
+    let doText = "Students Present (Do While Loop): ";
 
     do {
-        doText += j + " ";
+
+        doText += "Student" + j + " ";
+
         j++;
+
     } while(j <= 5);
 
     document.getElementById("doResult").innerHTML = doText;
